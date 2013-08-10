49d07ed9f799c190903b6755fb5a4eba86753872
Sameer Singh <sameer.singh@gmail.com>
2013-08-10 04:33:39 -0700

    refactored code and createFromExcel now creates or updates based on csid

--- createFromExcel.py
+++ createFromExcel.py
@@ -1,63 +1,75 @@
 import xlrd
 from createcontact3 import createUser3
-
-book = xlrd.open_workbook("/Users/sameersingh/Documents/Personal/Travel/CouchStatsNew.xls")
-
-K_GROUP="Murmansk"
-K_CSID_COL = 0
-K_FN_COL = 2
-K_LN_COL = 3
-K_AGE_COL = 4
-K_PHONE_COL = 5
-K_RATING_COL = 6
-K_NOTES_COL = 7
-
-sheet = book.sheet_by_name(K_GROUP)
-
-print sheet.name, sheet.nrows, sheet.ncols
-
-#for rx in range(sheet.nrows):
-#        print sheet.row(rx)
-
-for rx in range(sheet.nrows):
-
- print "len = " + str(len(sheet.cell_value(rx,K_CSID_COL).strip()))
- if len == 0:
-  continue 
-
- ### for phones check if it a number, if so, convert to string, strip trailing
- ### decimals and remove space in the numbers
-
- p = str(sheet.cell_value(rx,K_PHONE_COL))
- t = sheet.cell_type(rx,K_PHONE_COL)
- # Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
- if (t == 2) :
-   p = str(int(float(p)))
- p = "".join(p.split())
- p = "".join(c for c in p if c.isdigit() or c == "+")
- p = p.strip()
-
- print "p = " + p + "len = " + str(len(p))
- if len(p) == 0:
-   continue
-
- rating = sheet.cell_value(rx,K_RATING_COL)
- if rating == "":
-   rating = 4
- else:
-   rating = int(rating)
-  
-
- age = sheet.cell_value(rx,K_AGE_COL)
- if type(age) == str:
-   age =  "??"
- else:
-   age = int(age)
-   
- print "csid = " + sheet.cell_value(rx,K_CSID_COL) + "\n"
- print "name = " + sheet.cell_value(rx,K_FN_COL) + " " + str(rating) + str(age) + " " + sheet.name + "\n"
+from contactUtil import getGroupMembersDict
 
 
- notes = sheet.cell_value(rx,K_NOTES_COL)
+def createFromExcel (sheetName): 
 
- createUser3(sheet.cell_value(rx,2), str(rating)+str(age), p,sheet.cell_value(rx,K_CSID_COL),"Murmansk",notes)
+  K_GROUP=sheetName
+  csIDContactIDDict = getGroupMembersDict(K_GROUP)
+  book = xlrd.open_workbook("/Users/sameersingh/Documents/Personal/Travel/CouchStatsNew.xls")
+
+  K_CSID_COL = 0
+  K_FN_COL = 2
+  K_LN_COL = 3
+  K_AGE_COL = 4
+  K_PHONE_COL = 5
+  K_RATING_COL = 6
+  K_NOTES_COL = 7
+
+  sheet = book.sheet_by_name(K_GROUP)
+
+  print sheet.name, sheet.nrows, sheet.ncols
+
+  #for rx in range(sheet.nrows):
+  #        print sheet.row(rx)
+
+  for rx in range(sheet.nrows):
+
+   print "len = " + str(len(sheet.cell_value(rx,K_CSID_COL).strip()))
+   if len == 0:
+    continue 
+
+   ### for phones check if it a number, if so, convert to string, strip trailing
+   ### decimals and remove space in the numbers
+
+   p = str(sheet.cell_value(rx,K_PHONE_COL))
+   t = sheet.cell_type(rx,K_PHONE_COL)
+   # Cell Types: 0=Empty, 1=Text, 2=Number, 3=Date, 4=Boolean, 5=Error, 6=Blank
+   if (t == 2) :
+     p = str(int(float(p)))
+   p = "".join(p.split())
+   p = "".join(c for c in p if c.isdigit() or c == "+")
+   p = p.strip()
+
+   print "p = " + p + "len = " + str(len(p))
+   if len(p) == 0:
+     continue
+
+   rating = sheet.cell_value(rx,K_RATING_COL)
+   if rating == "":
+     rating = 4
+   else:
+     rating = int(rating)
+    
+
+   age = sheet.cell_value(rx,K_AGE_COL)
+   if type(age) == str:
+     age =  "??"
+   else:
+     age = int(age)
+     
+   print "csid = " + sheet.cell_value(rx,K_CSID_COL) + "\n"
+   print "name = " + sheet.cell_value(rx,K_FN_COL) + " " + str(rating) + str(age) + " " + sheet.name + "\n"
+
+
+   notes = sheet.cell_value(rx,K_NOTES_COL)
+
+   if sheet.cell_value(rx,K_CSID_COL) in csIDContactIDDict:
+     print "will update user " + sheet.cell_value(rx,2)
+   else:
+     print "will create user " + sheet.cell_value(rx,2)
+     createUser3(sheet.cell_value(rx,2), str(rating)+str(age) +  " " + K_GROUP, p,sheet.cell_value(rx,K_CSID_COL),K_GROUP,notes)
+
+
+createFromExcel("Murmansk")
---
+++ listcontactsbygroup.py
@@ -0,0 +1,60 @@
+import sha
+import atom
+import gdata.contacts
+import gdata.contacts.service
+from user import User
+from xml.dom import minidom
+from xml.etree.ElementTree import XML, fromstring, tostring
+
+K_GROUP='Murmansk'
+ 
+u = User()
+additional_headers = {'GData-Version' : '3.0' }
+gd_client = gdata.contacts.service.ContactsService(additional_headers = additional_headers)
+
+
+
+# Change this obviously 
+gd_client.email = u.user
+gd_client.password = u.pwd
+gd_client.ProgrammaticLogin()
+
+# readin all the groups
+groupIDDict = {}
+groups = gd_client.GetGroupsFeed()
+for g in groups.entry:
+  groupIDDict[g.title.text] = g.id.text
+  #print g.title.text
+
+"""
+This is used like a case statement later on.
+It's apparently a slightly slow way to do it but it just seems so elegant to me.  I'm not a Pythonista at all though so there may be a more Pythonic way to do it that I'm not aware of.
+""" 
+rel_list = {gdata.contacts.REL_WORK: "Work",
+            gdata.contacts.REL_HOME: "Home",
+            gdata.contacts.PHONE_WORK_FAX: "WorkFax",
+            gdata.contacts.PHONE_PAGER: "Pager",
+            gdata.contacts.REL_OTHER: "Other",
+            gdata.contacts.PHONE_MOBILE: "Mobile"}
+
+# This is the date used for "updated_min" below.
+date = '2009-08-12T00:00:00'
+ 
+query = gdata.contacts.service.ContactsQuery()
+query.feed='/m8/feeds/contacts/default/full?max_results=1000'
+query.max_results = 10000
+query.updated_min = date
+query.group = groupIDDict[K_GROUP]
+ 
+
+feed = gd_client.GetContactsFeed(query.ToUri())
+ 
+for contact in feed.entry:
+      print contact.website[0].href
+      print contact.id.text
+      print contact.title.text
+      for phone in contact.phone_number:
+        print rel_list[phone.rel] + ": " + phone.text
+ 
+def rel_type(rel):
+    pass
